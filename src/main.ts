45430d70f85adf11c3bf9f331fe5669f2fef7fff
voyagerwoo <voyager.woo@gmail.com>
2021-12-18 05:48:32 +0900

    Set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger.setup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true}));
+  setupSwagger(app);
   await app.listen(8080);
 }
 bootstrap();
